e581a8a603372d07ad94b1a033a0b66bdc6284d3
Luisa Ospina <desarrollo@academicos.cesde.edu.co>
2023-02-25 11:00:48 -0500

    hola

---
+++ OFFICE-WINDOWS.bat
@@ -0,0 +1,6 @@
+cd "C:\Program Files\Microsoft Office\Office16\"
+cscript OSPP.VBS /inpkey:3KJMN-HGKK8-2R2MG-DMPMQ-FM7QD
+cscript OSPP.VBS /act
+
+slmgr.vbs -ipk GXPKN-F3C2W-C693P-THCB2-76DGY
+slmgr.vbs -ato
